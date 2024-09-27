49a1080a0593aa65d6727f81ee35ac415e70687c
Jeffrey Hoelzel Jr <jmh2338@nau.edu>
2024-09-27 11:57:46 -0700

    Added feature for user input
    The user can input some text in the frontend and receive a message back from the backend.

--- service/app.py
+++ service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 # For testing only
@@ -7,9 +7,17 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
-@app.route('/')
-def hello():
-    return jsonify(message="Hello from the backend!")
+@app.route('/', methods=['GET', 'POST'])
+def send_message():
+    message = ''
+    # check if form is for post request
+    if request.method == 'POST':
+        # get message from user with name = demo in home.html
+        message = request.form.get('demo')
+        # modify message
+        message = f'We got your message: "{message}".'
+    # render new home.HTML page with modified message
+    return render_template('home.html', message=message)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
