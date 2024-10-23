d4dae0568b193f243f306ff842374df365a01c7c
William Barnett <will892@LAPTOP-GCQMH64J>
2024-10-23 15:29:17 -0700

    Moved files and made it run from new html

--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import './styles/styles1.css';
 import App from './App';
 
 // I am gonna be so real I don't know what I did
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
