9c1b01bf41a2377406dbaddfd486a559310d72f4
jeffreyHoelzel <jmh2338@nau.edu>
2024-10-24 14:30:35 -0700

    Added login popup to Login.js, working on getting index.js to work

--- web/src/Login.js
+++ web/src/Login.js
@@ -1,2 +1,46 @@
 // make sure to add to login-open to body classlist for preventing overflow
 // need to implement user sign out (html too)
+import {useState} from 'react';
+
+// style imports
+import '../src/styles/login.css';
+import '../src/styles/homepage.css';
+import './img/profile.jpeg';
+
+const Login = () => {
+  // set up use state for displaying login popup
+  const [loginPopup, setLoginPopup] = useState(false);
+
+  // allow popup to be toggleable
+  const toggleLoginPopup = () => {
+    setLoginPopup(!loginPopup);
+  };
+
+  return (
+    <div>
+      <div className="profile-box">
+        <a onClick={toggleLoginPopup}>
+          <img src="img/profile.jpeg" alt="Profile Icon" />
+        </a>
+      </div>
+      <div className="login-popup" id="popup">
+        <div className="login-popup-content">
+          <span className="close-popup">&times;</span>
+          <form action="" className="form-container">
+            <h2>Login</h2>
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter username" name="username" required />
+            <label for="password"><b>Password</b></label>
+            <input type="password" placeholder="Enter password" name="password" required />
+            <div className="option-container">
+              <button type="submit" className="login-btn">Login</button>
+              <a className="new-account" id="create-new-account">Register</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- web/src/index.js
+++ web/src/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import AddData from './DBExample';
+
+import { showLoginPopup } from './Login';
+
+//-------------- commenting out for now to test --------------//
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import './index.css';
+// import App from './App';
+// import AddData from './DBExample';
 
 // I am gonna be so real I don't know what I did
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/db_example"element={<AddData />} />
+//       </Routes>
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+//-------------- commenting out for now to test --------------//
+
+const popup = ReactDOM.createRoot(document.getElementById('login'));
+popup.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/db_example"element={<AddData />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Login />
+  </React.StrictMode>
 );
