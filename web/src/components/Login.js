739cdb30b37aba5d15cf223c60bd995a1709b1a3
jeffreyHoelzel <jmh2338@nau.edu>
2024-10-25 16:03:15 -0700

    Added function to handle user entering login info

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -1,8 +1,41 @@
 // style imports
 import '../styles/login.css';
 import '../styles/homepage.css';
+import { useState } from 'react';
 
 const Login = () => {
+  // set up use states for username, password, response message
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [valid, setValid] = useState(false);
+
+  // handle user submitting username and password
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // try to get response from backend
+    try {
+      const response = await fetch('service/login', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({query: username, query: password})
+      });
+
+      const data = await response.json();
+      console.log(data)
+      setMessage(data.message);
+      setValid(data.exists)
+    } catch (error) {
+      console.log("Error:", error);
+      setMessage("Username and password pair does not exist.");
+    }
+  }
+  // return boolean from backend if username, password kv pair exists
+  // if true, log in and display that they are logged in, otherwise, display error message
+
   return (
     <div>
       <form action="" className="form-container">
@@ -12,7 +45,7 @@ const Login = () => {
         <label for="password"><b>Password</b></label>
         <input type="password" placeholder="Enter password" name="password" required />
         <div className="option-container">
-          <button type="submit" className="login-btn">Login</button>
+          <button type="submit" className="login-btn" onClick={handleSubmit}>Login</button>
           <a className="new-account" id="create-new-account">Register</a>
         </div>
       </form>
