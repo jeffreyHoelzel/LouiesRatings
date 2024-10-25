5b8eb14b3840be43743ef2888bf268be6fce0ee4
jeffreyHoelzel <jmh2338@nau.edu>
2024-10-25 13:56:42 -0700

    Fixed indentation

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -5,20 +5,17 @@ import '../styles/homepage.css';
 const Login = () => {
   return (
     <div>
-      {/* <div className="login-popup" id="popup">
-        <div className="login-popup-content">
-          <span className="close-popup">&times;</span> */}
-          <form action="" className="form-container">
-            <h2>Login</h2>
-            <label for="username"><b>Username</b></label>
-            <input type="text" placeholder="Enter username" name="username" required />
-            <label for="password"><b>Password</b></label>
-            <input type="password" placeholder="Enter password" name="password" required />
-            <div className="option-container">
-              <button type="submit" className="login-btn">Login</button>
-              <a className="new-account" id="create-new-account">Register</a>
-            </div>
-          </form>
+      <form action="" className="form-container">
+        <h2>Login</h2>
+        <label for="username"><b>Username</b></label>
+        <input type="text" placeholder="Enter username" name="username" required />
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Enter password" name="password" required />
+        <div className="option-container">
+          <button type="submit" className="login-btn">Login</button>
+          <a className="new-account" id="create-new-account">Register</a>
+        </div>
+      </form>
     </div>
   );
 }
