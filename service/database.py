d4fd12d60771710d3eb34bacfdc5b78e3d53c574
jeffreyHoelzel <jmh2338@nau.edu>
2024-11-01 14:50:48 -0700

    Merge pull request #75 from jeffreyHoelzel/login

--- service/database.py
+++ service/database.py
@@ -12,7 +12,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(), nullable=False, unique=True)
