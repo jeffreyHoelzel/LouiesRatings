c2c2482114d7ddca48c196bc34b2b62cc6414703
William Barnett <will892@LAPTOP-GCQMH64J>
2024-11-22 22:20:20 -0700

    Edits that hopefully allow everyone to run the updated server

--- service/app.py
+++ service/app.py
@@ -253,4 +253,4 @@ def post_rating_route():
 # ====================================
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
