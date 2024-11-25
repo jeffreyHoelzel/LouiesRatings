27ef7d203cf82f43381cd7358187601146201389
ayluhrene <art386@nau.edu>
2024-11-24 19:42:56 -0700

    Revert "Added associated professors to class page with corresponding links"
    This reverts commit e9ffce6548f34097438032dab72dccde9ed4dfea.

--- service/graph_data.py
+++ service/graph_data.py
@@ -64,8 +64,3 @@ def get_graph_options(search_by, search_name):
     options += new_options
 
     return options
-
-def get_professor_list(search_name):
-    class_data = ClassData.query.filter_by(class_name=search_name).all()
-    professor_list = [data.instructor_name for data in class_data]
-    return sorted(list(set(professor_list)))  # Remove duplicates and sort
